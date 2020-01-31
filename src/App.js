b6e3517998357d81d191bc26063fae1be61918f4
Jeffrey Leung <jleung513@gmail.com>
2020-01-31 02:45:41 -0800

    Replace content with empty panes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+class Panes extends React.Component {
+  render() {
+    const paneStyling = {
+      height: "100vh",
+      
+      alignItems: "center",
+      justifyContent: "center",
+      textAlign: "center",
+      
+      overflow: "hidden",
+    };
+
+    return (
+      <div style={paneStyling}>
+        <PaneLeft />
+        <PaneRight />
+      </div>
+    );
+  }
+}
+
+class PaneLeft extends React.Component {
+  render() {
+    const styling = {
+      pane: {
+        height: "100vh",
+        width: "35vw",
+        display: "flex",
+        
+        float: "left",
+
+        alignItems: "center",
+        justifyContent: "center",
+
+      },
+
+      name: {
+        // display: "table-cell",
+        // verticalAlign: "middle",
+        // padding: "20vmin"
+      }
+    };
+
+    return (
+      <div style={styling.pane}>
+        <div style={styling.name}>
+          <p>Jeffrey Leung</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+class PaneRight extends React.Component {
+  render() {
+    const paneStyling = {
+      display: "flex",
+      float: "center",
+
+      alignItems: "center",
+      justifyContent: "center",
+      
+      overflowY: "always",
+    };
+
+    return (
+      <div style={paneStyling}>
+        <p>Content</p>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <div className="App-header">
+
+        <Panes />
+        
+      </div>
     </div>
   );
 }

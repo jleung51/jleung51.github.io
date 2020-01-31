02837942ff59314932e69f2648704f07dede7437
Jeffrey Leung <jleung513@gmail.com>
2020-01-31 02:46:58 -0800

    Add built files

---
+++ build/precache-manifest.91ee638268be283dcf4aebe53a7cdff6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5156c1a534bb3d63d39c02ed683403c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "acae666691430698d949",
+    "url": "/static/css/main.2ed951e3.chunk.css"
+  },
+  {
+    "revision": "07c5de2d504e7cf9a7df",
+    "url": "/static/js/2.a552a814.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.a552a814.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acae666691430698d949",
+    "url": "/static/js/main.ac41c172.chunk.js"
+  },
+  {
+    "revision": "6e7d85ad969107dd34f7",
+    "url": "/static/js/runtime-main.54edee9e.js"
+  }
+]);

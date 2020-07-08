a3f6dbb951824981ddc7632852c3079f26f22142
Jeffrey Leung <jleung513@gmail.com>
2020-07-07 21:34:49 -0700

    Update build

---
+++ build/precache-manifest.aeb2263dccca3f6cc05502ac520d6ddf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1afb56cdb6ca676956bd2ed1307c45b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "339d35a47f81bdd947c6",
+    "url": "/static/css/main.2540a5dd.chunk.css"
+  },
+  {
+    "revision": "d6c697a53713d56075c1",
+    "url": "/static/js/2.7ccba5b6.chunk.js"
+  },
+  {
+    "revision": "bc0495e3914ed396093e7a7990e70b00",
+    "url": "/static/js/2.7ccba5b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "339d35a47f81bdd947c6",
+    "url": "/static/js/main.bf463fc6.chunk.js"
+  },
+  {
+    "revision": "caf2a04453df4bace4e4",
+    "url": "/static/js/runtime-main.009173a0.js"
+  },
+  {
+    "revision": "cfa7895f20a8b228d90e946ab1235b1d",
+    "url": "/static/media/logo.cfa7895f.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f20b42425c549f319f1bdcadfc8f3954.js"
+  "/precache-manifest.aeb2263dccca3f6cc05502ac520d6ddf.js"
 );
 
 self.addEventListener('message', (event) => {
